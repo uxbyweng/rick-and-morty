abbaabcf5a3ee4f055bca8b91cada8037118c4f3
Karsten Weng <karsten@weng.eu>
2026-01-12 18:31:57 +0100

    feat(card): make card dynamic

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,26 +1,27 @@
-export function createCharacterCard() {
-  const card = document.createElement("li");
-  card.classList.add("card");
+export function createCharacterCard(characterName, characterImage, characterStatus, characterType, characterOccurency) {
+    // console.log(characterName, characterImage, characterStatus, characterType, characterOccurency);
+    const card = document.createElement("li");
+    card.classList.add("card");
 
-  card.innerHTML = `
+    card.innerHTML = `
               <div class="card__image-container">
             <img
               class="card__image"
-              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-              alt="Rick Sanchez"
+              src="${characterImage}"
+              alt="${characterName}"
             />
             <div class="card__image-gradient"></div>
           </div>
           <div class="card__content">
-            <h2 class="card__title">Rick Sanchez</h2>
+            <h2 class="card__title">${characterName}</h2>
             <dl class="card__info">
               <dt class="card__info-title">Status</dt>
-              <dd class="card__info-description">Alive</dd>
+              <dd class="card__info-description">${characterStatus}</dd>
               <dt class="card__info-title">Type</dt>
-              <dd class="card__info-description"></dd>
+              <dd class="card__info-description">${characterType}</dd>
               <dt class="card__info-title">Occurrences</dt>
-              <dd class="card__info-description">51</dd>
+              <dd class="card__info-description">${characterOccurency}</dd>
             </dl>`;
 
-  return card;
+    return card;
 }
--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
-
-const card = createCharacterCard();
-cardContainer.append(card);
 
 // States
 const maxPage = 1;
@@ -20,17 +15,28 @@ const searchQuery = "";
 
 // Fetch
 async function fetchCharacters() {
-  console.clear();
+    console.clear();
 
-  const response = await fetch("https://rickandmortyapi.com/api/character"); // first 20 Characters
-  // const response = await fetch("https://rickandmortyapi.com/api/character/?page=4");
-  const data = await response.json();
+    const response = await fetch("https://rickandmortyapi.com/api/character"); // first 20 Characters
+    const data = await response.json();
 
-  console.log(data.results);
+    cardContainer.innerHTML = "";
 
-  /* Funktion, die die Card erstellt und die Paramter übermittelt
-    createCharacterCard(data.results);
-    */
+    data.results.forEach((character) => {
+        const card = createCharacterCard(character.name, character.image, character.status, character.type, character.episode.length);
+        cardContainer.append(card);
+    });
+
+    // for (let i = 0; i < data.results.length; i++) {
+    //     const characterName = data.results[i].name;
+    //     const characterImage = data.results[i].image;
+    //     const characterStatus = data.results[i].status;
+    //     const characterType = data.results[i].type;
+    //     const characterOccurency = data.results[i].episode.length;
+
+    //     const card = createCharacterCard(characterName, characterImage, characterStatus, characterType, characterOccurency);
+    //     cardContainer.append(card);
+    // }
 }
 
 fetchCharacters();
