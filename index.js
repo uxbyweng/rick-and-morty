8cd13f5baa6ab8658f4d4fbb99ae5b19e928bc9c
Karsten Weng <karsten@weng.eu>
2026-01-12 15:06:48 +0100

    feat(api): fetch data from rick and morty api

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -12,3 +10,20 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// Fetch
+async function fetchCharacters() {
+    console.clear();
+
+    const response = await fetch("https://rickandmortyapi.com/api/character"); // first 20 Characters
+    // const response = await fetch("https://rickandmortyapi.com/api/character/?page=4");
+    const data = await response.json();
+
+    console.log(data.results);
+
+    /* Funktion, die die Card erstellt und die Paramter übermittelt
+    createCharacterCard(data.results);
+    */
+}
+
+fetchCharacters();
