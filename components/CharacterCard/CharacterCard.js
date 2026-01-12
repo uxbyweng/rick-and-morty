4f481da325aa2024e21dbf5e3b533d2f98671ff0
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-12 21:45:37 +0100

    refactor the code

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,65 +1,60 @@
 export function createCharacterCard(characters) {
+  const card = elementCreator("li", "card");
+  const imgContainer = elementCreator("div", "card__image-container");
 
-  const card = document.createElement("li");
-  card.classList.add("card");
-  console.log(characters);
+  const imgElement = elementCreator("img", "card__image");
+  imgElement.src = characters.image;
+  imgContainer.append(imgElement);
 
-  const imgContainer = document.createElement("div")
-  imgContainer.className = "card__image-container"
+  const cardGradient = elementCreator("div", "card__image-gradient");
 
-  const imgElement = document.createElement("img")
-  imgElement.className = "card__image"
-  imgElement.src = `${characters.image}`
-  imgContainer.append(imgElement)
+  const cardContent = elementCreator("div", "card__content");
 
-  card.append(imgContainer)
+  const h2 = elementCreator("h2", "card__title", characters.name);
 
-  const cardGradient = document.createElement("div")
-  cardGradient.className = "card__image-gradient"
-  card.append(cardGradient)
+  const cardInfo = elementCreator("dl", "card__info");
 
-  const cardContent = document.createElement("div")
-  cardContent.className = "card__content"
-  card.append(cardContent)
+  const statusTitle = elementCreator("dt", "card__info-title", "Status");
 
-  const h2 = document.createElement("h2")
-  h2.className = "card__title"
-  h2.textContent = characters.name
-  cardContent.append(h2)
+  const descriptionTitle = elementCreator(
+    "dd",
+    "card__info-description",
+    characters.status
+  );
 
-  const cardInfo = document.createElement("dl")
-  cardInfo.className = "card__info"
-  cardContent.append(cardInfo)
+  const typeTitle = elementCreator("dt", "card__info-title", "Type");
 
-  const statusTitle = document.createElement("dt")
-  statusTitle.className = "card__info-title"
-  statusTitle.textContent = "Status"
-  cardInfo.append(statusTitle)
+  const typeValue = elementCreator(
+    "dd",
+    "card__info-description",
+    characters.type
+  );
 
-  const descriptionTitle = document.createElement("dd")
-  descriptionTitle.className = "card__info-description"
-  descriptionTitle.textContent = characters.status
-  cardInfo.append(descriptionTitle)
-  
-  const typeTitle = document.createElement("dt")
-  typeTitle.className = "card__info-title"
-  typeTitle.textContent = "Type"
-  cardInfo.append(typeTitle)
+  const occurrence = elementCreator("dt", "card__infor-title", "Occurrences");
 
-  const typeValue = document.createElement("dd")
-  typeValue.className = "card__info-description"
-  typeValue.textContent = characters.type
-  cardInfo.append(typeValue)
+  const occurrenceValue = elementCreator(
+    "dd",
+    "card__info-description",
+    characters.episode.length
+  );
 
-  const occurrence = document.createElement("dt")
-  occurrence.className = "card__info-title"
-  occurrence.textContent = "Occurrences"
-  cardInfo.append(occurrence)
+  card.append(imgContainer, cardGradient, cardContent);
+  cardContent.append(h2, cardInfo);
+  cardInfo.append(
+    statusTitle,
+    descriptionTitle,
+    typeTitle,
+    typeValue,
+    occurrence,
+    occurrenceValue
+  );
 
-  const occurrenceValue = document.createElement("dd")
-  occurrenceValue.className = "card__info-description"
-  occurrenceValue.textContent = characters.episode.length
-  cardInfo.append(occurrenceValue)
-  
   return card;
+}
+//this function has three parameters first one is element tag, second is class and the third one is textcontent
+function elementCreator(elTag, className, content) {
+  const element = document.createElement(elTag);
+  element.className = className;
+  element.textContent = content;
+  return element;
 }
