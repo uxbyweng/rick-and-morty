9870e6aa880bf6e598998912fec4f299ab39022f
Karsten Weng <karsten@weng.eu>
2026-01-13 15:35:21 +0100

    refactor(searchbar): add favicon, outsource searchbar

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,11 +1,31 @@
-// components/SearchBar/SearchBar.js
-export function initSearchBar(searchBarElement, onSearch) {
+// SearchBar.js
+
+export function createSearchBar(searchBarContainer, onSearch) {
+    searchBarContainer.innerHTML = `
+    <form action="" class="search-bar" data-js="search-bar">
+      <input
+        name="query"
+        class="search-bar__input"
+        type="text"
+        placeholder="search characters"
+        aria-label="character name"
+      />
+      <button class="search-bar__button" aria-label="search for character">
+        <img
+          class="search-bar__icon"
+          src="assets/magnifying-glass.png"
+          alt=""
+        />
+      </button>
+    </form>
+  `;
+
+    const searchBarElement = searchBarContainer.querySelector('[data-js="search-bar"]');
+
     searchBarElement.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        const formData = new FormData(searchBarElement);
-        const query = formData.get("query").trim();
-
+        const query = searchBarElement.elements.query.value.trim();
         onSearch(query);
     });
 }
