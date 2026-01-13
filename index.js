10c92492386d4656f8beb6960babedc9810c114e
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-13 12:43:09 +0100

    feat: add fuctionality to next/pre buttons

--- index.js
+++ index.js
@@ -12,33 +12,43 @@ const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 let maxPage = 0;
-let page = 0;
-let character = 0;
+let page = 1;
 let searchQuery = "";
-nextButton.addEventListener("click", ()=> {
-  if (character >= 5){
-    page += 1
+
+console.log(maxPage);
+nextButton.addEventListener("click", () => {
+  if (page === maxPage) {
+    page = 1
+  }else{
+    page += 1;
+    fetchCharacters();
   }
-  character += 1
-  fetchCharacters()
-})
-prevButton.addEventListener("click", ()=> {
-  character -= 1
-  fetchCharacters()
-})
+});
+prevButton.addEventListener("click", () => {
+  if (page === 1) {
+    page = 42
+  }else{
+    page -= 1;
+    fetchCharacters();
+  }
+});
 // Fetch
 async function fetchCharacters() {
-    console.clear();
+  console.clear();
 
-    const response = await fetch("https://rickandmortyapi.com/api/character"); // first 20 Characters
-    const data = await response.json();
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  ); // first 20 Characters
+  const data = await response.json();
+  console.log(data);
+  maxPage = data.info.pages;
+  pagination.textContent = `${page} / ${maxPage}`;
+  cardContainer.innerHTML = "";
 
-    cardContainer.innerHTML = "";
-
-    data.results.forEach((character) => {
-        const card = createCharacterCard(character)
-        cardContainer.append(card);
-    });
+  data.results.forEach((character) => {
+    const card = createCharacterCard(character);
+    cardContainer.append(card);
+  });
 }
 fetchCharacters();
-
+console.log(maxPage);
