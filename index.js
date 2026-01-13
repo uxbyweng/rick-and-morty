20106532acb68e990bc4437970ba364272fb0cb5
Sergey Badin <sergeykfg1@me.com>
2026-01-13 11:26:58 +0100

    feat add pagination logic witch prev/next buttons and page state

--- index.js
+++ index.js
@@ -1,51 +1,86 @@
 import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-/* const card = createCharacterCard();
-cardContainer.append(card); */
-
-// States
+//=================
+// STATE
+//=================
 let maxPage = 1;
 let page = 1;
-let searchQuery = "";
+let characterIndex = 0;
+let characters = [];
+/* let searchQuery = ""; */
 
+// ================
 // Fetch
-async function fetchCharacters() {
-    console.clear();
+//=================
+async function fetchCharacters(startIndex = 0) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${page}`
+  ); // first 20 Characters
+  // const response = await fetch("https://rickandmortyapi.com/api/character/?page=4");
+  const data = await response.json();
 
-    const response = await fetch("https://rickandmortyapi.com/api/character"); // first 20 Characters
-    const data = await response.json();
+  console.log(data.results);
 
+  maxPage = data.info.pages;
+  characters = data.results;
+
+  characterIndex = startIndex;
+  renderCharacter();
+
+  //============================
+  // RENDER ONE CARD
+  //============================
+  function renderCharacter() {
     cardContainer.innerHTML = "";
 
-    data.results.forEach((character) => {
-        const card = createCharacterCard(character.name, character.image, character.status, character.type, character.episode.length);
-        cardContainer.append(card);
-    });
+    const charachter = characters[characterIndex];
+    cardContainer.append(createCharacterCard(charachter));
 
-    // for (let i = 0; i < data.results.length; i++) {
-    //     const characterName = data.results[i].name;
-    //     const characterImage = data.results[i].image;
-    //     const characterStatus = data.results[i].status;
-    //     const characterType = data.results[i].type;
-    //     const characterOccurency = data.results[i].episode.length;
+    pagination.textContent = `Page ${page} / ${maxPage} - character ${
+      characterIndex + 1
+    } / ${charachter.length}`;
+  }
 
-  cardContainer.innerHTML = "";
+  //==========================
+  // BUTTONS
+  //==========================
+  nextButton.onclick = () => {
+    if (characterIndex < characters.length - 1) {
+      characterIndex += 1;
+      renderCharacter();
+      return;
+    }
 
-  /* Funktion, die die Card erstellt und die Paramter übermittelt
-    createCharacterCard(data.results);
-    */
-  data.results.forEach((charachter) => {
-    const card = createCharacterCard(charachter);
-    cardContainer.append(card);
-  });
+    if (page < maxPage) {
+      page += 1;
+      fetchCharacters(0);
+    }
+  };
+
+  prevButton.onclick = () => {
+    if (characterIndex > 0) {
+      characterIndex -= 1;
+      renderCharacter();
+      return;
+    }
+
+    if (page > 1) {
+      page -= 1;
+      fetchCharacters(19);
+    }
+  };
 }
-
+//================
+// LOAD
+//================
 fetchCharacters();
