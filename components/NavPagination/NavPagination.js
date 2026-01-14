ae553b21de571fb26948cdb6df58aac0cdd25938
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-14 20:32:43 +0100

    refactor error handler

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -1,16 +1,16 @@
-function createNavPagination(className= "navigation__pagination", attribute="pagination",content="1/1"){
-  const span = document.createElement("span")
-  span.className = className
-  span.setAttribute("data-jsx", attribute)
-  span.textContent = content
-  return span
+function createNavPagination(
+  className = "navigation__pagination",
+  attribute = "pagination",
+  content = "1/1"
+) {
+  const span = document.createElement("span");
+  span.className = className;
+  span.setAttribute("data-jsx", attribute);
+  span.textContent = content;
+  return span;
 }
-function noResult(container, element){
-  container.innerHTML = "";
-  paginationElement.textContent = "1 / 1";
-
-  const li = document.createElement("li");
-  li.textContent = "No characters found.";
-  element.append(li);
+function noResult(container, element, message) {
+  container.innerHTML = message;
+  element.textContent = "0/0";
 }
-export {createNavPagination, noResult}
+export { createNavPagination, noResult };
