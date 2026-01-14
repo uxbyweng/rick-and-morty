6f21322af0f7afcea1dce7866ade317b345f6df5
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-14 16:14:59 +0100

    refactor the code

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -1,11 +1,11 @@
-export function createNavPagination(className= "navigation__pagination", attribute="pagination",content="1/1"){
+function createNavPagination(className= "navigation__pagination", attribute="pagination",content="1/1"){
   const span = document.createElement("span")
   span.className = className
   span.setAttribute("data-jsx", attribute)
   span.textContent = content
   return span
 }
-export function noResult(container, element){
+function noResult(container, element){
   container.innerHTML = "";
   paginationElement.textContent = "1 / 1";
 
@@ -13,3 +13,4 @@ export function noResult(container, element){
   li.textContent = "No characters found.";
   element.append(li);
 }
+export {createNavPagination, noResult}
